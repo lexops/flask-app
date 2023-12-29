3797cd7a6eb1572e3ef10e670d74c229d22f2e35
Alex Silva <134613820+lexops@users.noreply.github.com>
2023-12-29 10:40:20 -0300

    Red App

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', hostname=hostname)
+    return render_template('red.html', hostname=hostname)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
